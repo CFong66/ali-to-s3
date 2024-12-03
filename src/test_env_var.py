df5b6ee2d97a032942a9e92a191573fe0029feb5
CFong66 <chrisfang11@gmail.com>
2024-12-03 16:06:05 +1100

    update test

--- src/test_env_var.py
+++ src/test_env_var.py
@@ -1,9 +1,11 @@
 import os
+from dotenv import load_dotenv
 
-# Retrieve environment variables
+# Load environment variables from the .env file
+load_dotenv()
+
 SNS_TOPIC_ARN = os.getenv('SNS_TOPIC_ARN')
 SQS_QUEUE_URL = os.getenv('SQS_QUEUE_URL')
 
-# Check if the values are retrieved correctly
 print(f"SNS_TOPIC_ARN: {SNS_TOPIC_ARN}")
 print(f"SQS_QUEUE_URL: {SQS_QUEUE_URL}")
