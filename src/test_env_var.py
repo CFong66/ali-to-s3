e2a3f4b2d956b4faffe6670938adc085e9811383
CFong66 <chrisfang11@gmail.com>
2024-12-04 18:27:13 +1100

    u

--- src/test_env_var.py
+++ src/test_env_var.py
@@ -1,11 +1,12 @@
 import os
-# from dotenv import load_dotenv
 
-# # Load environment variables from the .env file
-# load_dotenv()
+# Retrieve environment variables
+sns_topic_arn = os.getenv('SNS_TOPIC_ARN')
+sqs_queue_url = os.getenv('SQS_QUEUE_URL')
+ali_access_key_id = os.getenv('ALI_ACCESS_KEY_ID')
+ali_access_key_secret = os.getenv('ALI_ACCESS_KEY_SECRET')
 
-SNS_TOPIC_ARN = os.getenv('SNS_TOPIC_ARN')
-SQS_QUEUE_URL = os.getenv('SQS_QUEUE_URL')
-
-print(f"SNS_TOPIC_ARN: {SNS_TOPIC_ARN}")
-print(f"SQS_QUEUE_URL: {SQS_QUEUE_URL}")
+print(f"SNS Topic ARN: {sns_topic_arn}")
+print(f"SQS Queue URL: {sqs_queue_url}")
+print(f"Ali Access Key ID: {ali_access_key_id}")
+print(f"Ali Access Key Secret: {ali_access_key_secret}")
