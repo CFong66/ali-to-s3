1046a819b2ce68d8aab563af4f0f8957cfc6cb47
CFong66 <chrisfang11@gmail.com>
2024-12-04 16:38:33 +1100

    update test env var

--- src/test_env_var.py
+++ src/test_env_var.py
@@ -1,8 +1,8 @@
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-# Load environment variables from the .env file
-load_dotenv()
+# # Load environment variables from the .env file
+# load_dotenv()
 
 SNS_TOPIC_ARN = os.getenv('SNS_TOPIC_ARN')
 SQS_QUEUE_URL = os.getenv('SQS_QUEUE_URL')
