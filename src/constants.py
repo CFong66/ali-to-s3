d3c9b0d3d3fc10bab10b67fa2965d66a8754be52
CFong66 <chrisfang11@gmail.com>
2024-12-04 16:27:55 +1100

    asdf

--- src/constants.py
+++ src/constants.py
@@ -11,3 +11,13 @@ LOG_BUCKET = 'ali-vt-log-bucket'
 DYNAMODB_TABLE = 'video_metadata'
 VIDEO_BUCKET_FOLDER = 'ali-videos'
 S3_METADATA_PATH = 'video-metadata/metadata.json' 
+
+
+"""
+flag for rclone to multipart upload
+  --s3-upload-cutoff 300M \
+  --s3-chunk-size 16M \
+  --s3-upload-concurrency 8 \
+  --transfers 4 \
+  --progress
+"""
